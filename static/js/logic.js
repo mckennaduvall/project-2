a4e774a3a061e191d4e762a59a063616e59ae4af
Christopher Tran <ChristopherTran94@gmail.com>
2021-07-17 10:29:49 -0700

    states

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,3 +14,64 @@ var myMap = L.map("map", {
     accessToken: API_KEY
   }).addTo(myMap);
   
+  function chooseColor(borough) {
+    switch (borough) {
+    case "Brooklyn":
+      return "yellow";
+    case "Bronx":
+      return "red";
+    case "Manhattan":
+      return "orange";
+    case "Queens":
+      return "green";
+    case "Staten Island":
+      return "purple";
+    default:
+      return "black";
+    }
+  }
+
+var link = "/static/data/US_States.json"
+
+  d3.json(link).then(function(data) {
+    // Creating a geoJSON layer with the retrieved data
+    L.geoJson(data, {
+      // Style each feature (in this case a neighborhood)
+      style: function(feature) {
+        return {
+          color: "white",
+          // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
+          fillColor: chooseColor(feature.properties.NAME),
+          fillOpacity: 0.5,
+          weight: 1.5
+        };
+      },
+      // Called on each feature
+      onEachFeature: function(feature, layer) {
+        // Set mouse events to change map styling
+        layer.on({
+          // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+          mouseover: function(event) {
+            layer = event.target;
+            layer.setStyle({
+              fillOpacity: 0.9
+            });
+          },
+          // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+          mouseout: function(event) {
+            layer = event.target;
+            layer.setStyle({
+              fillOpacity: 0.5
+            });
+          },
+          // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+          click: function(event) {
+            myMap.fitBounds(event.target.getBounds());
+          }
+        });
+        // Giving each feature a pop-up with information pertinent to it
+        layer.bindPopup("<h1>" + feature.properties.neighborhood + "</h1> <hr> <h2>" + feature.properties.borough + "</h2>");
+  
+      }
+    }).addTo(myMap);
+  });
